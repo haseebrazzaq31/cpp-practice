85490d99a9b7624ab2d5e93b132a7d3e3ed358b0
Haseeb Razzaq <f2025-0848@bnu.edu.pk>
2025-12-18 19:37:59 +0500

    Add files via upload

---
+++ area_finder.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int length;
+    int width;
+    int area;
+    cout<<"Enter length "<<endl;
+    cin>>length;
+    cout<<"Enter width "<<endl;
+    cin>>width;
+    area=length*width;
+    cout<<"Area = "<<area<<endl;
+    return 0;
+}
